9a5d18c2e3ac4e3151220aefdb00bab525461318
apefood <kabelonkakala20@gmail.com>
2023-09-27 02:09:25 +0200

    basic magazine layout - fine

---
+++ widgets/templates/magazine-listing.php
@@ -0,0 +1,132 @@
+<!-- header container --> 
+<div class="flex w-full h-44 md:h-80">
+    <!-- cover heading --> 
+    <div class="bg-[#BF0000] w-full">
+        <h2 class="text-center text-white text-2xl uppercase md:text-4xl py-16 md:py-36">The Plug Magazine</h2>
+    </div>
+</div>
+
+<!-- posts listing container -->
+<div class="flex flex-col w-full mt-1 md:m-2 p-2">
+
+    <!-- single post (thumb left) --> 
+    <div class="flex flex-col md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+    </div>
+
+    <!-- single post (thumb right) --> 
+    <div class="flex flex-col-reverse md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+    </div>
+
+    <!-- single post (thumb left) --> 
+    <div class="flex flex-col md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+    </div>
+
+    <!-- single post (thumb right) --> 
+    <div class="flex flex-col-reverse md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+    </div>
+
+    <!-- single post (thumb left) --> 
+    <div class="flex flex-col md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+    </div>
+
+    <!-- single post (thumb right) --> 
+    <div class="flex flex-col-reverse md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+    </div>
+
+    <!-- single post (thumb left) --> 
+    <div class="flex flex-col md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+    </div>
+
+    <!-- single post (thumb right) --> 
+    <div class="flex flex-col-reverse md:flex-row w-full mb-5 pb-5 border-b md:mx-2">
+        
+        <!-- post info --> 
+        <div class="w-full md:w-1/2 md:ml-2 py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ea illo, pariatur sapiente laboriosam voluptates magni rerum, dolore ducimus repudiandae earum debitis libero itaque eius sequi adipisci. Ex, quis porro?
+        </div>
+
+        <!-- thumbnail --> 
+        <div class="w-full md:w-1/2 md:h-80 md:mr-2 bg-black py-40 text-center text-white">
+            THUMBNAIL
+        </div>
+
+    </div>
+
+</div>
